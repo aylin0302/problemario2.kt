fbf07e95f9e1fb582504731437e44aafa963f4b4
aylin0302 <aylinroa89@gmail.com>
2024-11-27 22:04:37 -0600

    problema9.kt

---
+++ problema9.kt
@@ -0,0 +1,16 @@
+#9. Optimización de funciones: Escribe un programa que encuentre el
+#mínimo o máximo de una función cuadrática mediante el cálculo de su
+#derivada.
+
+from sympy import solve
+
+def optimizacion_cuadratica(expresion):
+    x = symbols("x")
+    derivada = diff(expresion, x)
+    critico = solve(derivada, x)
+    return critico, derivada
+
+#Ejemplo:
+expresion = "x**2 - 8*x + 3"
+criticos, derivada = optimizacion_cuadratica(expresion)
+print(f"Puntos críticos: {criticos}, Derivada: {derivada}")
