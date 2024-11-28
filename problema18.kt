1a7b0be02c11bd1a9b5579cbf4f9a07bb493312e
aylin0302 <aylinroa89@gmail.com>
2024-11-27 23:02:32 -0600

    problema18.kt

---
+++ problema18.kt
@@ -0,0 +1,11 @@
+#19. Número de caminos: Calcula el número de caminos posibles en una
+#cuadrícula NxN, moviéndose solo hacia la derecha o hacia abajo.
+
+from math import comb
+
+def caminos_cuadricula(n):
+    return comb(2 * n, n)
+
+#Ejemplo:
+n = 6
+print(f"Número de caminos en una cuadrícula {n}x{n}: {caminos_cuadricula(n)}")
