3fddf899dfec5f1a1a5cb1ba0be137854b247475
aylin0302 <aylinroa89@gmail.com>
2024-11-27 23:34:56 -0600

    problema10.kt

--- problema10.kt
+++ problema10.kt
@@ -9,7 +9,7 @@ def integral_trapecio(funcion, a, b, n=1000):
         suma += funcion(a + i * h)
     return suma * h
 
-#Ejemplo:
+
 funcion = lambda x: x**4
 a, b = 0, 2
 print(f"Integral definida de x^4 en [{a}, {b}]: {integral_trapecio(funcion, a, b)}")
