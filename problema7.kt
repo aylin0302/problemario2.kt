93217a55ad1af3001f27adc8d3f01b93a5a0d817
aylin0302 <aylinroa89@gmail.com>
2024-11-27 21:59:30 -0600

    problema7.kt

---
+++ problema7.kt
@@ -0,0 +1,14 @@
+#7. Derivadas simbólicas: Diseña una función que acepte una expresión
+#algebraica (por ejemplo, 3x^2 + 2x) y calcule su derivada simbólica.
+
+from sympy import symbols, diff
+
+def derivada_simbolica(expresion, variable):
+    x = symbols(variable)
+    derivada = diff(expresion, x)
+    return derivada
+
+#Ejemplo:
+derivada = "2*x**2 + 2*x"
+variable = "x"
+print(f"Derivada de '{derivada}': {derivada_simbolica(derivada, variable)}")
